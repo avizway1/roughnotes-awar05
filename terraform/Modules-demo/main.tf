bc9e3fa9c55d0540ab17c3382acdb51f6c2db5df
Avinash <avizway@avizacademy.com>
2025-12-02 10:54:28 +0530

    awar05-notes

--- terraform/Modules-demo/main.tf
+++ terraform/Modules-demo/main.tf
@@ -4,12 +4,8 @@ provider "aws" {
 
 module "ec2_instance" {
   source        = "./modules/ec2"
-  instance_type = var.instance_type
-  ami           = var.ami
-  size          = var.size
 }
 
 module "s3_bucket" {
   source   = "./modules/s3"
-  mybucket = var.mybucket
 }
